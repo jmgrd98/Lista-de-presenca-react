b1bdcdc6bf87df9dfd5cf8dbdfb03a4d0a5e8e8a
João Marcelo Dantas <jmgrd98@gmail.com>
2022-08-19 10:57:46 -0300

    index.js root commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './lista-de-presenca/src/App';
+import reportWebVitals from './lista-de-presenca/src/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
